6e56d3f9730ff7e4637a63de7bc169066e0ad7e1
parigoswami <goswamiutangpari@yahoo.in>
2018-03-25 20:23:12 +0530

    init commit

---
+++ temp.py
@@ -0,0 +1,42 @@
+x = "Hello atom"
+x
+import numpy as np
+X = np.array([[1,0,1,0],[1,0,1,1],[0,1,0,1]])
+y = np.array([[1],[1],[0]])
+def sigmoid (x):
+    return 1/(1 + np.exp(-x))
+def derivatives_sigmoid (x):
+    return x*(1-x)
+epoch=5000
+lr=0.1
+inputlayer_neurons = X.shape[1]  #4
+hiddenlayer_neurons = 3
+output_neurons = 1
+
+wh=np.random.uniform(size=(inputlayer_neurons,hiddenlayer_neurons))
+bh=np.random.uniform(size=(1,hiddenlayer_neurons))
+wout=np.random.uniform(size=(hiddenlayer_neurons,output_neurons))
+bout=np.random.uniform(size=(1,output_neurons))
+
+for i in range(epoch):
+        #Forward Propogation
+        hidden_layer_input1=np.dot(X,wh)
+        hidden_layer_input=hidden_layer_input1 + bh
+        hiddenlayer_activations = sigmoid(hidden_layer_input)
+        output_layer_input1=np.dot(hiddenlayer_activations,wout)
+        output_layer_input= output_layer_input1+ bout
+        output = sigmoid(output_layer_input)
+
+        #Backpropagation
+        E = y-output
+        slope_output_layer = derivatives_sigmoid(output)
+        slope_hidden_layer = derivatives_sigmoid(hiddenlayer_activations)
+        d_output = E * slope_output_layer
+        Error_at_hidden_layer = d_output.dot(wout.T)
+        d_hiddenlayer = Error_at_hidden_layer * slope_hidden_layer
+        wout += hiddenlayer_activations.T.dot(d_output) *lr
+        bout += np.sum(d_output, axis=0,keepdims=True) *lr
+        wh += X.T.dot(d_hiddenlayer) *lr
+        bh += np.sum(d_hiddenlayer, axis=0,keepdims=True) *lr
+
+print (output)
